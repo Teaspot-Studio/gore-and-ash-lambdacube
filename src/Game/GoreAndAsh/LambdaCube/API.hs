0ef29deeee510ef3ab3c4d4f1ae9ad192af1bb02
NCrashed <NCrashed@gmail.com>
2016-02-10 04:43:14 +0300

    Pipeline register/unregister API

--- src/Game/GoreAndAsh/LambdaCube/API.hs
+++ src/Game/GoreAndAsh/LambdaCube/API.hs
@@ -9,25 +9,81 @@ Portability : POSIX
 -}
 module Game.GoreAndAsh.LambdaCube.API(
     MonadLambdaCube(..)
+  , LambdaCubeException(..)
   ) where
 
+import Control.DeepSeq
+import GHC.Generics 
+
+import Control.Exception.Base (Exception)
+import Control.Monad.Catch (throwM, MonadThrow)
 import Control.Monad.State.Strict 
 import Control.Monad.Trans 
-import LambdaCube.GL as LambdaCubeG
+import Control.Monad.Writer (Writer)
+
+import LambdaCube.Compiler as LambdaCube
+import LambdaCube.GL as LambdaCubeGL
 
 import Game.GoreAndAsh.LambdaCube.Module 
 import Game.GoreAndAsh.LambdaCube.State 
 
+-- | Exception type that could be thrown by the module
+data LambdaCubeException =
+  -- | Thrown when a pipeline compilation failed, first is pipeline main module, last is error message
+    PipeLineCompileFailed String PipelineId String
+  -- | Thrown when tries to register the same pipeline twice
+  | PipeLineAlreadyRegistered PipelineId
+  -- | Thrown when failed to bind pipeline to context, contains pipeline name and error message
+  | PipeLineIncompatible PipelineId String
+  deriving (Generic, Show)
+
+instance Exception LambdaCubeException
+
 -- | Low level monadic API for module.
-class MonadIO m => MonadLambdaCube m where 
+class (MonadIO m, MonadThrow m) => MonadLambdaCube m where 
   -- | Update viewport size for rendering engine
   -- Should be called when window size is changed (or every frame)
-  lambdacubeUpdateSize :: Int -> Int -> m ()
+  lambdacubeUpdateSize :: Word -- ^ Width of screen in pixels
+    -> Word -- ^ Height of screen in pixels 
+    -> m ()
 
-instance {-# OVERLAPPING #-} MonadIO m => MonadLambdaCube (LambdaCubeT s m) where
+  -- | Compile and register new pipeline.
+  --
+  -- Throws: 'PipeLineCompileFailed' or 'PipeLineAlreadyRegistered' when failed.
+  lambdacubeAddPipeline :: 
+    [FilePath] -- ^ Where to find LC modules
+    -> String -- ^ Name of main module (without of .lc)
+    -> PipelineId -- ^ Name of pipeline to register
+    -> Writer PipelineSchema a -- ^ Pipeline inputs description
+    -> m ()
+
+  -- | Removes pipeline from engine, deallocates all storages for rendering storages
+  --
+  -- Note: if pipeline with the name doesn't exists, do nothing.
+  lambdacubeDeletePipeline :: PipelineId -> m ()
+
+instance {-# OVERLAPPING #-} (MonadIO m, MonadThrow m) => MonadLambdaCube (LambdaCubeT s m) where
   lambdacubeUpdateSize w h = do 
     s <- get 
     liftIO $ updateStateViewportSize w h s
 
-instance {-# OVERLAPPABLE #-} (MonadIO (mt m), MonadLambdaCube m, MonadTrans mt) => MonadLambdaCube (mt m) where 
+  lambdacubeAddPipeline ps mn pid pwr = do 
+    s <- get 
+    when (isPipelineRegisteredInternal pid s) . throwM . PipeLineAlreadyRegistered $! pid
+    mpd <- liftIO $ LambdaCube.compileMain ["."] OpenGL33 "hello"
+    case mpd of 
+      Left err -> throwM . PipeLineCompileFailed mn pid $! "compile error:\n" ++ err
+      Right pd -> do 
+        let sch = makeSchema pwr 
+        r <- liftIO $ LambdaCubeGL.allocRenderer pd
+        put $! registerPipelineInternal pid pd sch r s
+
+  lambdacubeDeletePipeline i = do 
+    s <- get 
+    s' <- liftIO $ unregisterPipelineInternal i s 
+    put s'
+
+instance {-# OVERLAPPABLE #-} (MonadIO (mt m), MonadThrow (mt m), MonadLambdaCube m, MonadTrans mt) => MonadLambdaCube (mt m) where 
   lambdacubeUpdateSize a b = lift $ lambdacubeUpdateSize a b
+  lambdacubeAddPipeline a b c d = lift $ lambdacubeAddPipeline a b c d
+  lambdacubeDeletePipeline a = lift $ lambdacubeDeletePipeline a
--- src/Game/GoreAndAsh/LambdaCube/State.hs
+++ src/Game/GoreAndAsh/LambdaCube/State.hs
@@ -14,7 +14,11 @@ module Game.GoreAndAsh.LambdaCube.State(
   , StorageId(..)
   , emptyLambdaCubeState
   , freeLambdaCubeState
+  -- | Internal API
   , updateStateViewportSize
+  , isPipelineRegisteredInternal
+  , registerPipelineInternal
+  , unregisterPipelineInternal
   ) where
 
 import Control.DeepSeq
@@ -28,6 +32,7 @@ import qualified Data.HashMap.Strict as H
 import Data.Sequence (Seq)
 import qualified Data.Sequence as S 
 
+import LambdaCube.Compiler as LambdaCube
 import LambdaCube.GL as LambdaCubeGL
 
 -- | ID to uniquely identify LambdaCube rednering pipeline
@@ -43,17 +48,32 @@ data StorageId = StorageId {
 instance NFData StorageId 
 instance Hashable StorageId
 
+-- | Check is storage binded to specified pipeline
+isPipelineStorage :: PipelineId -> StorageId -> Bool 
+isPipelineStorage pid sid = storageScheme sid == pid
+
+-- | All info ablut pipeline
+data PipelineInfo = PipelineInfo {
+  pipeInfoRenderer :: !GLRenderer
+, pipeInfoSchema :: !PipelineSchema
+, pipeInfoPipeline :: !Pipeline   
+} deriving Generic 
+
+instance NFData PipelineInfo
+
 -- | Internal state of core module
 --
 -- [@s@] - state of next module, they are chained until bottom, that is usually
 --         an empty data type.
 data LambdaCubeState s = LambdaCubeState {
   -- | Module storage for LambdaCube pipelines
-  lambdacubePipelines :: !(HashMap PipelineId (PipelineSchema, GLRenderer))
+  lambdacubePipelines :: !(HashMap PipelineId PipelineInfo)
   -- | Module storage for LambdaCube storages
 , lambdacubeStorages :: !(HashMap StorageId GLStorage)
   -- | Defines in which order to render each object at next frame
 , lambdacubeRenderOrder :: !(Seq StorageId)
+  -- | Next storage id to use
+, lambdacubeNextStorageId :: !Int
   -- | Next module state in chain of modules
 , lambdacubeNextState :: !s
 } deriving (Generic)
@@ -69,6 +89,9 @@ instance NFData GLStorage where
 instance NFData GLRenderer where 
   rnf a = a `seq` () 
 
+instance NFData Pipeline where 
+  rnf a = a `seq` () 
+
 -- | Create inital state of the core module
 --
 -- [@s@] -  state of next module
@@ -77,6 +100,7 @@ emptyLambdaCubeState s = LambdaCubeState {
     lambdacubePipelines = H.empty 
   , lambdacubeStorages = H.empty 
   , lambdacubeRenderOrder = S.empty
+  , lambdacubeNextStorageId = 0
   , lambdacubeNextState = s
   }
 
@@ -84,9 +108,41 @@ emptyLambdaCubeState s = LambdaCubeState {
 freeLambdaCubeState :: LambdaCubeState s -> IO ()
 freeLambdaCubeState LambdaCubeState{..} = do 
   mapM_ LambdaCubeGL.disposeStorage lambdacubeStorages
-  mapM_ (LambdaCubeGL.disposeRenderer . snd) lambdacubePipelines
+  mapM_ (LambdaCubeGL.disposeRenderer . pipeInfoRenderer) lambdacubePipelines
 
 -- | Update viewport size of all storages
-updateStateViewportSize :: Int -> Int -> LambdaCubeState s -> IO ()
+updateStateViewportSize :: Word -> Word -> LambdaCubeState s -> IO ()
 updateStateViewportSize w h LambdaCubeState{..} = 
-  mapM_ (\s -> LambdaCubeGL.setScreenSize s (fromIntegral w) (fromIntegral h)) $ H.elems lambdacubeStorages
+  mapM_ (\s -> LambdaCubeGL.setScreenSize s w h) $ H.elems lambdacubeStorages
+
+-- | Returns True if given pipeline is already exists
+isPipelineRegisteredInternal :: PipelineId -> LambdaCubeState s -> Bool 
+isPipelineRegisteredInternal pid LambdaCubeState{..} = case H.lookup pid lambdacubePipelines of 
+    Nothing -> False 
+    Just _ -> True 
+
+-- | Register new pipeline with renderer in module
+registerPipelineInternal :: PipelineId -> Pipeline -> PipelineSchema -> GLRenderer -> LambdaCubeState s -> LambdaCubeState s
+registerPipelineInternal i ps pl r s = s {
+    lambdacubePipelines = H.insert i info . lambdacubePipelines $! s
+  }
+  where 
+    info = PipelineInfo {
+        pipeInfoRenderer = r 
+      , pipeInfoSchema = pl
+      , pipeInfoPipeline = ps
+      }
+
+-- | Removes pipeline from state and deletes it, also destroys all storages of the pipeline
+unregisterPipelineInternal :: PipelineId -> LambdaCubeState s -> IO (LambdaCubeState s)
+unregisterPipelineInternal i s =
+  case H.lookup i . lambdacubePipelines $! s of 
+    Nothing -> return s
+    Just (PipelineInfo{..}) -> do 
+      let storages = H.filterWithKey (\k _ -> isPipelineStorage i k) . lambdacubeStorages $! s
+      mapM_ LambdaCubeGL.disposeStorage . H.elems $! storages
+      LambdaCubeGL.disposeRenderer pipeInfoRenderer 
+      return $ s {
+          lambdacubePipelines = H.delete i . lambdacubePipelines $! s
+        , lambdacubeStorages = H.filterWithKey (\k _ -> not $ isPipelineStorage i k) . lambdacubeStorages $! s
+        }
