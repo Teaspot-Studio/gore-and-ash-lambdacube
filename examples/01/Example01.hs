fcb048159f5cbd543e0aac54925e01d490e1a6f4
Anton Gushcha <ncrashed@gmail.com>
2017-01-23 16:23:16 +0300

    Restore examples structure and place SDL example as temporary

--- examples/01/Example01.hs
+++ examples/01/Example01.hs
@@ -3,19 +3,51 @@ module Main where
 import Control.Monad.Catch
 import Control.Monad.Except
 import Data.Proxy
+import Foreign.C
+import SDL (get)
 
 import Game.GoreAndAsh.Core
-import Game.GoreAndAsh.Logging
 import Game.GoreAndAsh.LambdaCube
+import Game.GoreAndAsh.Logging
+import Game.GoreAndAsh.SDL
 import Game.GoreAndAsh.Time
 
-type AppStack t = LambdaCubeT t (LoggingT t (TimerT t (GameMonad t)))
+type AppStack t = LambdaCubeT t (SDLT t (LoggingT t (TimerT t (GameMonad t))))
 
 newtype AppMonad t a = AppMonad { runAppMonad :: AppStack t a}
   deriving (Functor, Applicative, Monad, MonadFix)
 
-app :: forall t m . (MonadLambdaCube t m) => m ()
-app = exampleFunc
+drawFrame :: forall t . (ReflexHost t, MonadIO (HostFrame t))
+  => Window -> Renderer -> HostFrame t ()
+drawFrame win r = do
+  rendererDrawColor r $= V4 0 0 0 0
+  clear r
+  rendererDrawColor r $= V4 250 0 0 0
+  ws <- getCurrentSize
+  let squareRect :: Rectangle Double
+      squareRect = Rectangle (P $ V2 0.1 0.1) (V2 0.8 0.8)
+  fillRect r (Just $ resizeRect ws squareRect)
+  glSwapWindow win
+  where
+    getCurrentSize :: HostFrame t (V2 CInt)
+    getCurrentSize = do
+      vp <- get (rendererViewport r)
+      case vp of
+        Nothing -> return 0
+        Just (Rectangle _ s) -> return s
+
+    resizeRect :: V2 CInt -> Rectangle Double -> Rectangle CInt
+    resizeRect (V2 vw vh) (Rectangle (P (V2 x y)) (V2 w h)) = Rectangle (P (V2 x' y')) (V2 w' h')
+      where
+        x' = round $ x * fromIntegral vw
+        y' = round $ y * fromIntegral vh
+        w' = round $ w * fromIntegral vw
+        h' = round $ h * fromIntegral vh
+
+app :: forall t m . (MonadLambdaCube t m, MonadSDL t m) => m ()
+app = do
+  _ <- createMainWindow never drawFrame defaultWindowCfg
+  return ()
 
 main :: IO ()
 main = runSpiderHost $ hostApp $ runModule opts (app :: AppMonad Spider ())
@@ -34,6 +66,7 @@ deriving instance (ReflexHost t, MonadIO (HostFrame t)) => MonadIO (AppMonad t)
 deriving instance (ReflexHost t, MonadIO (HostFrame t), MonadThrow (HostFrame t)) => MonadLambdaCube t (AppMonad t)
 deriving instance (ReflexHost t, MonadIO (HostFrame t)) => TimerMonad t (AppMonad t)
 deriving instance (ReflexHost t, MonadIO (HostFrame t)) => LoggingMonad t (AppMonad t)
+deriving instance (ReflexHost t, MonadIO (HostFrame t), MonadCatch (HostFrame t)) => MonadSDL t (AppMonad t)
 deriving instance (ReflexHost t) => MonadSample t (AppMonad t)
 deriving instance (ReflexHost t) => MonadHold t (AppMonad t)
 deriving instance (ReflexHost t) => MonadSubscribeEvent t (AppMonad t)
